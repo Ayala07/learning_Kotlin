343bcbb971cf257b3c794f25ff0507dc73caf063
Roberto Ayala <robertube26@gmail.com>
2021-02-15 18:41:56 -0600

    Agregando funcion para separar palabras

--- app/src/main/java/com/example/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learningkotlin/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.learningkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        proccessButton.setOnClickListener {
+            val sentence = inputTxt.text.toString()
+            separated(sentence)
+        }
+    }
+
+    private fun separated(sentence:String){
+        var response = ""
+        for (i in sentence){
+            response += "$i "
+        }
+
+        resLbl.text = response
     }
 }
