84adf2727a9988caad5cf316770b5267a24f1a4d
Roberto Ayala <robertube26@gmail.com>
2021-02-15 19:47:38 -0600

    Agregando funcion para añadir mayusculas y minusculas aleatorias

--- app/src/main/java/com/example/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learningkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.learningkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
 
         proccessButton.setOnClickListener {
             val sentence = inputTxt.text.toString()
-            separated(sentence)
+            //separated(sentence)
+            upperAndLowerCasse(sentence)
         }
     }
 
@@ -19,6 +21,20 @@ class MainActivity : AppCompatActivity() {
         var response = ""
         for (i in sentence){
             response += "$i "
+        }
+
+        resLbl.text = response
+    }
+
+    private fun upperAndLowerCasse(sentence: String){
+        var response = ""
+        for (i in sentence){
+            val random = Random.nextBoolean()
+            response += if (random){
+                i.toUpperCase()
+            } else {
+                i.toLowerCase()
+            }
         }
 
         resLbl.text = response
